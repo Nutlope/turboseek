5e6613b636abee0a8f7587d460841b1c8b3a8c83
Sam Selikoff <sam.selikoff@gmail.com>
2024-06-06 17:03:59 -0600

    wip

--- app/api/getAnswer/route.ts
+++ app/api/getAnswer/route.ts
@@ -48,27 +48,10 @@ export async function POST(request: Request) {
     }),
   );
 
-  let debug = finalResults.map(
-    (result, index) => `[[citation:${index}]] ${result.fullContent} \n\n`,
-  );
-  console.log("DEBUG LENGTH");
-  console.log(debug.toString().length);
-
-  const mainAnswerPrompt = `
-  Given a user question and some context, please write a clean, concise and accurate answer to the question based on the context. You will be given a set of related contexts to the question, each starting with a reference number like [[citation:x]], where x is a number. Please use the context when crafting your answer.
-
-  Your answer must be correct, accurate and written by an expert using an unbiased and professional tone. Please limit to 1024 tokens. Do not give any information that is not related to the question, and do not repeat. Say "information is missing on" followed by the related topic, if the given context do not provide sufficient information.
-
-  Here are the set of contexts:
-
-  <contexts>
-  ${finalResults.map(
-    (result, index) => `[[citation:${index}]] ${result.fullContent} \n\n`,
-  )}
-  </contexts>
-
-  Remember, don't blindly repeat the contexts verbatim and don't tell the user how you used the citations – just respond with the answer. It is very important for my career that you follow these instructions. Here is the user question:
-    `;
+  const mainAnswerPrompt = Array.from(Array(10000).keys())
+    .map(() => `Lorem ipsum`)
+    .join("\n");
+  console.log(mainAnswerPrompt.length);
 
   try {
     const payload: TogetherAIStreamPayload = {
